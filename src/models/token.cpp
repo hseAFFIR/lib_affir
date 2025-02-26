0c71230f067f46d393e9110b5402887b4ec5ed8e
Shon Qwezon <shonzbso@gmail.com>
2025-02-26 14:41:31 +0300

    feat: add token class

---
+++ src/models/token.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Shon on 26.02.2025.
+//
+
+#include "token.h"
---
+++ src/models/token.h
@@ -0,0 +1,14 @@
+//
+// Created by Shon on 26.02.2025.
+//
+
+#ifndef LIB_AFFIR_TOKEN_H
+#define LIB_AFFIR_TOKEN_H
+
+
+class Token {
+
+};
+
+
+#endif //LIB_AFFIR_TOKEN_H
