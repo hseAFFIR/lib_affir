b1fa093fe01361dd037c39989f23a5009260a178
Shon <62799691+shonqwezon@users.noreply.github.com>
2025-03-12 23:50:51 +0300

    feat: add FileStorage (#1)
    * chore: init FileStorage
    
    * chore: init add constructors and write func
    
    * fix: multi writing
    
    * feat: add read func
    
    * feat: add help func for read
    
    * feat: add caching storage meta
    
    * refactor: delete redundant vars
    
    * docs: add doc comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include "storages/fileStorage.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+//    std::string str = "1234,5687,(),1234,4567,()";
+//    std::string str2 = "asdf,1234,asdf,ery,()1234";
+//    auto *fileStorage = new FileStorage("MyFile", str.size());
+//    auto *fileStorage2 = new FileStorage("MyFile", str2.size());
+//    fileStorage2->write(str2);
+//    fileStorage->write(str);
+//    fileStorage->close();
+//    fileStorage2->close();
+
+    auto *fileStorage3 = new FileStorage(1);
+
+    std::vector<char> buffer;
+    while (!fileStorage3->isEnd()) {
+        fileStorage3->read(buffer);
+        std::cout << buffer.data() << std::endl;
+    }
+    FileStorage::saveStorageMeta();
     return 0;
 }
---
+++ src/storages/fileStorage.cpp
@@ -0,0 +1,181 @@
+#include <stdexcept>
+#include <iostream>
+#include <cerrno>
+#include <cstring>
+#include <format>
+#include "fileStorage.h"
+
+unsigned short FileStorage::instancesNumber = 0;
+unsigned long FileStorage::g_cursor = 0;
+std::unordered_map<FileId, DataStruct> FileStorage::dataMap;
+
+bool FileStorage::isStorageLoaded = false;
+const std::string FileStorage::DATA_FILENAME_PATH = "storage_raw";
+const std::string FileStorage::META_FILENAME_PATH = "storage_metadata.bin";
+
+FileStorage::FileStorage(const std::string &filename, const size_t filesize) {
+
+    loadStorageMeta();
+
+    instancesNumber++;
+    id = dataMap.size() + 1;
+    dataStruct = DataStruct(g_cursor, filename, filesize);
+    dataMap[id] = dataStruct;
+    open();
+
+    std::cout << std::format("Created file block with: filename = {}, startPos = {}, endPos = {}, id = {}",
+                             dataStruct.filename, dataStruct.startPos, dataStruct.endPos(), id) << std::endl;
+
+    dataStream.seekp((long) dataStruct.startPos);
+    g_cursor = dataStruct.endPos();
+}
+
+FileStorage::FileStorage(const FileId fileId) : id(fileId) {
+    loadStorageMeta();
+
+    open();
+    dataStruct = dataMap[id];
+    currentPosition = 0;
+    std::cout << std::format("Created file block with: filename = {}, startPos = {}, endPos = {}, id = {}",
+                             dataStruct.filename, dataStruct.startPos, dataStruct.endPos(), id) << std::endl;
+}
+
+void FileStorage::open() {
+    if (dataStream.is_open()) return;
+    createDataFile();
+    dataStream.open(DATA_FILENAME_PATH, std::ios::out | std::ios::in);
+}
+
+FileStorage::~FileStorage() {
+    instancesNumber--;
+}
+
+void FileStorage::write(std::string_view data) {
+    dataStream.write(data.data(), (long)data.size());
+    std::streampos position = dataStream.tellp();
+    std::cout << std::format("New pos (file {}) = {}", dataStruct.filename, (long)position) << std::endl;
+    if (position > dataStruct.endPos())
+        throw std::out_of_range(std::format("position > dataStruct.endPos, {} > {}", (long)position, dataStruct.endPos()));
+}
+
+size_t FileStorage::read(std::vector<char> &buffer, size_t bytesToRead, size_t startPos) {
+    if (startPos >= dataStruct.filesize)
+        throw std::out_of_range("Starting position exceeds file's end position.");
+
+    dataStream.seekg((long) startPos);
+
+    buffer.resize(bytesToRead + 1);
+    size_t bytesToEnd = dataStruct.endPos() - dataStream.tellg();
+    dataStream.read(buffer.data(), (long)std::min(bytesToRead, bytesToEnd));
+
+    size_t bytesRead = dataStream.gcount();
+    std::cout << std::format("Read {} bytes from position {}", bytesRead, startPos) << std::endl;
+    return bytesRead;
+}
+
+size_t FileStorage::read(std::vector<char> &buffer, size_t bytesToRead) {
+    size_t bytesRead = read(buffer, bytesToRead, currentPosition);
+    currentPosition += bytesToRead;
+    return bytesRead;
+}
+
+void FileStorage::close() {
+    dataStream.close();
+    delete this;
+}
+
+FileId FileStorage::getId() const {
+    return id;
+}
+
+size_t FileStorage::getFilesize() const {
+    return dataStruct.filesize;
+}
+
+std::string FileStorage::getFilename() const {
+    return dataStruct.filename;
+}
+
+void FileStorage::createDataFile() {
+    dataStream.open(DATA_FILENAME_PATH, std::ios::app);
+    if (!dataStream.is_open())
+        throw std::runtime_error("Cannot open file: " + std::string(strerror(errno)));
+    dataStream.close();
+}
+
+bool FileStorage::isEnd() {
+    return dataStream.tellg() == dataStruct.endPos();
+}
+
+void FileStorage::loadStorageMeta() {
+    if (isStorageLoaded) return;
+
+    std::ifstream metaFileIn(FileStorage::META_FILENAME_PATH, std::ios::binary);
+    if (!metaFileIn.is_open()) {
+        std::cerr << "Cannot open file: " + std::string(strerror(errno)) << std::endl;
+        return;
+    }
+
+    // Load cursor
+    metaFileIn.read(reinterpret_cast<char*>(&g_cursor), sizeof(g_cursor));
+
+    // Load size of dataMap
+    size_t mapSize;
+    metaFileIn.read(reinterpret_cast<char*>(&mapSize), sizeof(mapSize));
+
+    dataMap.clear();
+    for (size_t i = 0; i < mapSize; ++i) {
+        // Read FileId
+        FileId fileId;
+        metaFileIn.read(reinterpret_cast<char*>(&fileId), sizeof(fileId));
+
+        // Read filesize
+        DataStruct data;
+        metaFileIn.read(reinterpret_cast<char*>(&data.startPos), sizeof(data.startPos));
+
+        // Read filename length
+        size_t filenameSize;
+        metaFileIn.read(reinterpret_cast<char*>(&filenameSize), sizeof(filenameSize));
+        // Read filename
+        data.filename.resize(filenameSize);
+        metaFileIn.read(&data.filename[0], (long) filenameSize);
+
+        // Read filesize
+        metaFileIn.read(reinterpret_cast<char*>(&data.filesize), sizeof(data.filesize));
+
+        dataMap[fileId] = data;
+    }
+    metaFileIn.close();
+    isStorageLoaded = true;
+}
+
+void FileStorage::saveStorageMeta() {
+    std::ofstream metaFileOut(FileStorage::META_FILENAME_PATH, std::ios::binary);
+
+    // Save cursor
+    metaFileOut.write(reinterpret_cast<char*>(&g_cursor), sizeof(g_cursor));
+
+    // Save size of dataMap
+    size_t mapSize = dataMap.size();
+    metaFileOut.write(reinterpret_cast<char*>(&mapSize), sizeof(mapSize));
+
+    // Save each element of the unordered_map
+    for (const auto& pair : dataMap) {
+        // Write FileId
+        metaFileOut.write(reinterpret_cast<const char*>(&pair.first), sizeof(pair.first));
+
+        // Save DataStruct members
+        metaFileOut.write(reinterpret_cast<const char*>(&pair.second.startPos), sizeof(pair.second.startPos));
+
+        size_t filenameSize = pair.second.filename.size();
+        // Write length of filename
+        metaFileOut.write(reinterpret_cast<char*>(&filenameSize), sizeof(filenameSize));
+        // Write filename
+        metaFileOut.write(pair.second.filename.c_str(), (long)filenameSize);
+
+        // Write filesize
+        metaFileOut.write(reinterpret_cast<const char*>(&pair.second.filesize), sizeof(pair.second.filesize));
+    }
+    metaFileOut.close();
+}
+
