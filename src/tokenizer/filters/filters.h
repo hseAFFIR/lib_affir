6b0db60e8ed30e9cc322ef000299224f3d98bc3a
defonisek <defonisek@mail.ru>
2025-03-13 13:31:21 +0300

    refactor: filled in filters.h with existing filters

---
+++ src/tokenizer/filters/filters.h
@@ -0,0 +1,10 @@
+#ifndef LIB_AFFIR_FILTERS_H
+#define LIB_AFFIR_FILTERS_H
+
+#include "base.h"
+#include "htmler.h"
+#include "lowercaser.h"
+#include "punctuator.h"
+#include "stopWords.h"
+
+#endif
