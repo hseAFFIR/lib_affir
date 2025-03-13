3c73d4d441a31a2e94141df67a3bcecb12aad1d1
Olesia <o.koshurinova@gmail.com>
2025-03-14 01:33:50 +0300

    fix: update `Token` class to use `unsigned long`

--- src/models/token.cpp
+++ src/models/token.cpp
@@ -8,19 +8,19 @@
 // Реализация конструкторов
 Token::Token() : body(""), pos(0), index(0), fileId(0){}
 
-Token::Token(const std::string& body, long long pos, long long index,long long fileId)
+Token::Token(const std::string& body, unsigned long pos, unsigned long index,unsigned long fileId)
     : body(body), pos(pos), index(index), fileId(fileId){}
 
 // Геттеры
 std::string Token::getBody() const { return body; }
-long long Token::getPos() const { return pos; }
-long long Token::getIndex() const { return index; }
-long long Token::getFileId() const { return fileId; }
+unsigned long Token::getPos() const { return pos; }
+unsigned long Token::getIndex() const { return index; }
+unsigned long Token::getFileId() const { return fileId; }
 
 // Сеттеры
 void Token::setBody(const std::string& newBody) { body = newBody; }
-void Token::setPos(long long newPos) { pos = newPos; }
-void Token::setIndex(long long newIndex) { index = newIndex; }
+void Token::setPos(unsigned long newPos) { pos = newPos; }
+void Token::setIndex(unsigned long newIndex) { index = newIndex; }
 
 // Перегрузка оператора вывода в поток
 std::ostream& operator<<(std::ostream& os, const Token& token) {
--- src/models/token.h
+++ src/models/token.h
@@ -7,25 +7,25 @@
 class Token {
 private:
     std::string body;  // Слово
-    long long pos;     // Позиция в тексте
-    long long index;   // Позиция среди токенов
-    long long fileId; //
+    unsigned long pos;     // Позиция в тексте
+    unsigned long index;   // Позиция среди токенов
+    unsigned long fileId; //
 
 public:
     // Конструкторы
     Token();  // Конструктор по умолчанию
-    Token(const std::string& body, long long pos, long long index, long long fileId);  // Параметризованный конструктор
+    Token(const std::string& body, unsigned long pos, unsigned long index, unsigned long fileId);  // Параметризованный конструктор
 
     // Геттеры
     std::string getBody() const;
-    long long getPos() const;
-    long long getIndex() const;
-    long long getFileId() const;
+    unsigned long getPos() const;
+    unsigned long getIndex() const;
+    unsigned long getFileId() const;
 
     // Сеттеры
     void setBody(const std::string& newBody);
-    void setPos(long long newPos);
-    void setIndex(long long newIndex);
+    void setPos(unsigned long newPos);
+    void setIndex(unsigned long newIndex);
 
     // Перегрузка оператора сравнения
     bool operator==(const Token& other) const;
