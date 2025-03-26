4168d752bb8a077073a0b450963ac27b85d929f6
Shon <62799691+shonqwezon@users.noreply.github.com>
2025-03-26 11:05:09 +0300

    docs: add usage examples to file storage (#7)
    * chore: delete redundancy
    
    * chore: add doc to IStorageIndex + rename it
    
    * docs: add usage examples to filestorage
    
    * style: rename class

--- src/common.h
+++ src/common.h
@@ -6,10 +6,10 @@
 #ifndef LIB_AFFIR_COMMON_H
 #define LIB_AFFIR_COMMON_H
 
+#include <unordered_map>
+#include <vector>
 #include "models/tokenInfo.h"
 
-#include <unordered_map>  // Добавьте эту строку
-#include <vector>         // Если ещё не подключен
 // Type alias for FileId to represent file identifier.
 typedef unsigned long FileId;
 typedef unsigned long Pos;
