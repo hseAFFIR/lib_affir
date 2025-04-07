17e560a1e6877ae7b016f69151cbca5ab5bcc9a0
Shon Qwezon <shonzbso@gmail.com>
2025-04-07 16:35:59 +0300

    refactor: move TokenInfo to common

--- src/common.h
+++ src/common.h
@@ -8,12 +8,14 @@
 
 #include <unordered_map>
 #include <vector>
-#include "models/tokenInfo.h"
 #include <cstdint>
 
 // Type alias for FileId to represent file identifier.
 typedef uint64_t FileId;
 typedef uint64_t Pos;
+struct TokenInfo {
+    Pos pos;    ///< The absolute position in the data stream.
+    Pos wordPos;///< The position within a word or sentence.
+};
 typedef std::unordered_map<FileId, std::vector<TokenInfo>> PosMap;
-
 #endif //LIB_AFFIR_COMMON_H
