306bf580d649bd7527885060a71284bb0902f118
GithubDenis5 <gromacihindenis@gmail.com>
2025-04-09 23:13:00 +0300

    test: add process test without fileStorage

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -8,6 +8,7 @@
 #include "../processing/dataHandler.h"
 #include "../storages/indexes/multi/multiFileIndexStorage.h"
 #include "../storages/indexes/iIndexStorage.h"
+#include "../indexer/indexer.h"
 
 namespace fs = std::filesystem;
 /**
@@ -54,6 +55,57 @@ void runTestProcessingWithoutReadTime(const std::string& folderPath, size_t buff
     for (auto filter : filters) {
         delete filter;
     }
+}
+
+
+void runTestTokIndIndStor(const std::string& folderPath, size_t buffer ,std::vector<Base*> &filters, IIndexStorage& storage) {
+    // Загружаем все файлы заранее, чтобы не учитывать время чтения
+    std::unordered_map<std::string, std::string> fileContents;
+
+    for (const auto& entry : fs::directory_iterator(folderPath)) {
+        if (entry.is_regular_file()) {
+            std::ifstream file(entry.path());
+            if (!file) {
+                std::cerr << "Could not open file: " << entry.path() << std::endl;
+                continue;
+            }
+
+            std::string text((std::istreambuf_iterator<char>(file)),
+                             std::istreambuf_iterator<char>());
+            std::string filename = entry.path().filename().string();
+            fileContents[filename] = std::move(text);
+        }
+    }
+
+    FileId id = 0;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (const auto& [filename, text] : fileContents) {
+        Tokenizer tk(filters);
+        Indexer ind(buffer, storage);
+
+        tk.tokenizeFiltered(text, [&ind](Token token) {
+
+            ind.addToken(token);
+        }, id);
+
+        ind.saveTo();
+        storage.saveStorageMeta();
+
+        id++;
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+
+    std::cout << "Total Tokenizer+Indexer+IndexStorage time (buffer - "<< buffer <<" bites}): "
+              << diff.count() << " seconds\n";
+
+    for (auto filter : filters) {
+        delete filter;
+    }
+
 }
 
 #if defined(_WIN32)
