0fbb1c7a32c59667d9963a5260774a117c1f6367
GithubDenis5 <gromacihindenis@gmail.com>
2025-04-10 11:19:54 +0300

    fix: delete filters delete

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -52,9 +52,6 @@ void runTestProcessingWithoutReadTime(const std::string& folderPath, size_t buff
     std::cout << "Total processing time (buffer - "<< buffer <<" bites}): "
               << diff.count() << " seconds\n";
 
-    for (auto filter : filters) {
-        delete filter;
-    }
 }
 
 
@@ -101,10 +98,6 @@ void runTestTokIndIndStor(const std::string& folderPath, size_t buffer ,std::vec
 
     std::cout << "Total Tokenizer+Indexer+IndexStorage time (buffer - "<< buffer <<" bites}): "
               << diff.count() << " seconds\n";
-
-    for (auto filter : filters) {
-        delete filter;
-    }
 
 }
 
@@ -161,6 +154,53 @@ void printPeakMemoryUsage() {
     }
 #endif
 
+
+
+
+size_t countOccurrences(const std::string& text, const std::string& word) {
+    size_t count = 0;
+    size_t pos = 0;
+    while ((pos = text.find(word, pos)) != std::string::npos) {
+        ++count;
+        pos += word.length();
+    }
+    return count;
+}
+
+void searchWordInFiles(const std::string& folderPath, const std::string& word) {
+    std::unordered_map<std::string, std::string> fileContents;
+
+    // Загрузка всех файлов заранее
+    for (const auto &entry: fs::directory_iterator(folderPath)) {
+        if (entry.is_regular_file()) {
+            std::ifstream file(entry.path());
+            if (!file) {
+                std::cerr << "Could not open file: " << entry.path() << std::endl;
+                continue;
+            }
+
+            std::string text((std::istreambuf_iterator<char>(file)),
+                             std::istreambuf_iterator<char>());
+            std::string filename = entry.path().filename().string();
+            fileContents[filename] = std::move(text);
+        }
+    }
+
+    // Засекаем время поиска
+    auto start = std::chrono::high_resolution_clock::now();
+
+    size_t totalOccurrences = 0;
+    for (const auto &[filename, content]: fileContents) {
+        size_t count = countOccurrences(content, word);
+        totalOccurrences += count;
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = end - start;
+
+    std::cout << "Total occurrences of \"" << word << "\": " << totalOccurrences << '\n';
+    std::cout << "Search time: " << elapsed.count() << " seconds\n";
+}
 
 
 
