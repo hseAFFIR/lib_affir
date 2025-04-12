9085235134cca91d4a0a1e45c037c364fef839e1
Shon Qwezon <shonzbso@gmail.com>
2025-04-13 01:15:11 +0300

    fix: add default values

--- src/common.h
+++ src/common.h
@@ -10,14 +10,14 @@ using FileId = uint64_t;
 using Pos = uint64_t;
 
 struct TokenInfo {
-    Pos pos;    ///< The absolute position in the data stream.
-    Pos wordPos;///< The position within a word or sentence.
+    Pos pos{};    ///< The absolute position in the data stream.
+    Pos wordPos{};///< The position within a word or sentence.
 };
 
 struct Token {
     std::string body;
-    TokenInfo info;
-    FileId fileId;
+    TokenInfo info{};
+    FileId fileId{};
 };
 
 using PosMap = std::unordered_map<FileId, std::vector<TokenInfo>>;
