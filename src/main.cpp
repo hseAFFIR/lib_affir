1c3513acc1ab4243c664267ee7e2e0a7f65ebb72
Shon Qwezon <shonzbso@gmail.com>
2025-04-13 01:16:07 +0300

    chore: add example

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,37 @@
 #include "indexer/indexer.h"
 #include "searcher/search.h"
 #include "logger/logger.h"
+#include "processing/dataHandler.h"
+#include "tokenizer/filters/htmler.h"
+#include "tokenizer/filters/punctuator.h"
+#include "tokenizer/filters/stopWords.h"
 
 
 int main() {
     Logger::init("logs/log.txt");
     Logger::info("Main", "Application started");
 
+    std::vector<Base*> filters = { new Htmler(), new Punctuator(),
+                                   new StopWords()};
+
     MultiFileIndexStorage storage;
-    Indexer indexer(1024, storage);
-    Search searcher(indexer);
+    DataHandler dataHandler(filters, 1024*1024*16, storage);
 
-    // Search for a single word
-    auto results = searcher.search("fox");
+    std::string str = "This ebook is for the use of anyone anywhere in the United States and\n"
+                      "most other parts of the world at no cost and with almost no restrictions\n"
+                      "whatsoever. You may copy it, give it away or re-use it under the terms\n"
+                      "of the Project Gutenberg License included with this ebook or online\n"
+                      "at www.gutenberg.org. If you are not located in the United States,\n"
+                      "you will have to check the laws of the country where you are located\n"
+                      "before using this eBook.";
 
-    // Print results using built-in function
-    searcher.printSearchResults(results);
+    dataHandler.processText(str, "file1");
 
+//    Search search(filters, storage);
+//    std::string find = "parts of the world";
+//    auto res = search.search(find);
+//    Search::printSearchResults(res);
+
+    for (auto f : filters) delete f;
     return 0;
 }
