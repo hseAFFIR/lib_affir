3134ec90fc9ec7a14501e0a431f281c0d2617778
amenk <spyssasin@gmail.com>
2025-04-16 22:20:20 +0300

    fix(logger): async_logger

--- tests/main.cpp
+++ tests/main.cpp
@@ -8,18 +8,24 @@
 
 #include "utils/utils.h"
 #include "tests.h"
+#include "spdlog/async.h"
 
 int main() {
     system("chcp 65001"); // Для корректного отображения русского языка в логах
-    Logger::init("logs/test.txt");
+//    Logger::init("logs/test.txt");
+    auto async_logger = spdlog::basic_logger_mt<spdlog::async_factory>("async_log", "logs/async.txt");
+
     Logger::info("Tests", "Application started");
+
+
+
     std::vector<Base*> filters = { new Htmler(), new Punctuator(),
                                    new StopWords()};
 
 
     MultiFileIndexStorage mfis;
 
-    runTestProcessingWithoutReadTime("test_data", 1024*1024*16, filters, mfis);
+    runTestProcessingWithoutReadTime("C:\\rep\\lib_affir\\tests\\final_dataset", 1024*1024*16, filters, mfis);
 
     printPeakMemoryUsage();
 
