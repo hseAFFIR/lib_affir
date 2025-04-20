4d555fe7c434e246067442fbfcd98a537c10615a
Shon Qwezon <shonzbso@gmail.com>
2025-04-20 19:55:16 +0300

    docs: update example in main

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,25 @@ int main() {
     Logger::init("logs/log.txt");
     Logger::info("Main", "Application started");
 
-    Engine engine(FilterType::HTMLER | FilterType::LOWERCASER, IndexStorageType::MULTI);
-    // Или без фильтров: Engine engine(Engine::IndexStorageType::SINGLE); // Либо можно передать FilterType::NONE
+    Engine engine(EngineFocus::POSES, FilterType::HTMLER | FilterType::LOWERCASER, IndexStorageType::MULTI);
+    // Или без фильтров: Engine engine(EngineFocus::NATIVE, Engine::IndexStorageType::SINGLE); // Либо можно передать FilterType::NONE
 
-//    engine.proceed("Западная граница Карелии является частью государственной границы Российской Федерации и Финляндии. На востоке Карелия граничит с Архангельской областью, на юге — с Вологодской и Ленинградской областями, на севере — с Мурманской областью.", "Some file");
+    engine.proceed("Западная граница Карелии является частью государственной границы Российской Федерации и Финляндии. На востоке Карелия граничит с Архангельской областью, на юге — с Вологодской и Ленинградской областями, на севере — с Мурманской областью.", "Some file");
 
     // Или передать путь до файла. Вторым параметром по желанию можно указать CHUNK_SIZE
 //    engine.proceed("/path/to/file");
 
+    FileId fileId = 1;
+    engine.openFS(fileId);
+    std::vector<char> buffer;
+    engine.read(buffer);
+    Logger::info("Main", "Read 512 bytes of the given file ({}):\n{}", fileId, buffer.data());
+
     auto res = engine.find("Карелия");
-    engine.openFS(1);
     Logger::info("Main", "Result:\n{}", res.toString());
-    Logger::info("Main", "{}", Engine::returnResultInContext(res, 20)[1].front());
+
+    size_t contextSymbols = 20;
+    Logger::info("Main", "Result in context in file ({}): {}", fileId, Engine::returnResultInContext(res, contextSymbols)[fileId].front());
 
     return 0;
 }
